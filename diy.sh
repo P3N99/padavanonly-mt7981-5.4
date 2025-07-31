905cc9791926fe2c13f23f81f425d2aff657e4f2
P3N99 <145282517+P3N99@users.noreply.github.com>
2025-07-31 14:02:02 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -12,14 +12,13 @@ svn_export() {
 	rm -rf "$TMP_DIR"
 }
 
-find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
-find ./ | grep Makefile | grep mosdns | xargs rm -f
+# find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
+# find ./ | grep Makefile | grep mosdns | xargs rm -f
 
 rm -rf ./feeds/packages/lang/golang 
 git clone https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
 rm -rf ./feeds/luci/applications/luci-app-argon-config
 rm -rf ./feeds/luci/themes/luci-theme-argon
-
 
 git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
@@ -38,13 +37,3 @@ sed -i 's/ImmortalWrt/MT7981/' package/base-files/files/bin/config_generate
 sed -i "s/key='.*'/key=123456789/g" ./package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
 sed -i "s/country='.*'/country='CN'/g" ./package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
 # sed -i "s/encryption='.*'/encryption='sae-mixed'/g" ./package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
-
-# DNS劫持
-# sed -i '/dns_redirect/d' package/network/services/dnsmasq/files/dhcp.conf
-# adguardhome
-VER=$(grep PKG_VERSION package/luci-app-adguardhome/Makefile | sed 's/-/\./g')
-sed -i "s/PKG_VERSION:=.*/$VER/g" package/luci-app-adguardhome/Makefile
-cd package
-# 汉化
-curl -sfL -o ./convert_translation.sh https://github.com/kenzok8/small-package/raw/main/.github/diy/convert_translation.sh
-chmod +x ./convert_translation.sh && bash -v ./convert_translation.sh
